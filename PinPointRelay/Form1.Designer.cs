28d021430cacaa9406604d86e972457b54470260
FootKjott <oscarmccully@gmail.com>
2017-02-18 21:14:52 -0500

    initial commit

---
+++ PinPointRelay/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace PinPointRelay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.RefreshButton = new System.Windows.Forms.Button();
+            this.COMPortsList = new System.Windows.Forms.ListBox();
+            this.LogBox = new System.Windows.Forms.TextBox();
+            this.WebServerAddressBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SysTrayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // RefreshButton
+            // 
+            this.RefreshButton.Location = new System.Drawing.Point(234, 9);
+            this.RefreshButton.Name = "RefreshButton";
+            this.RefreshButton.Size = new System.Drawing.Size(75, 23);
+            this.RefreshButton.TabIndex = 0;
+            this.RefreshButton.Text = "Refresh";
+            this.RefreshButton.UseVisualStyleBackColor = true;
+            this.RefreshButton.Click += new System.EventHandler(this.RefreshButton_Click);
+            // 
+            // COMPortsList
+            // 
+            this.COMPortsList.FormattingEnabled = true;
+            this.COMPortsList.Location = new System.Drawing.Point(108, 9);
+            this.COMPortsList.Name = "COMPortsList";
+            this.COMPortsList.Size = new System.Drawing.Size(120, 69);
+            this.COMPortsList.TabIndex = 1;
+            this.COMPortsList.SelectedValueChanged += new System.EventHandler(this.COMPortsList_SelectedValueChanged);
+            // 
+            // LogBox
+            // 
+            this.LogBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.LogBox.Location = new System.Drawing.Point(0, 136);
+            this.LogBox.Multiline = true;
+            this.LogBox.Name = "LogBox";
+            this.LogBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.LogBox.Size = new System.Drawing.Size(436, 117);
+            this.LogBox.TabIndex = 2;
+            // 
+            // WebServerAddressBox
+            // 
+            this.WebServerAddressBox.Location = new System.Drawing.Point(12, 110);
+            this.WebServerAddressBox.Name = "WebServerAddressBox";
+            this.WebServerAddressBox.Size = new System.Drawing.Size(412, 20);
+            this.WebServerAddressBox.TabIndex = 3;
+            this.WebServerAddressBox.TextChanged += new System.EventHandler(this.WebServerAddressBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Destination web server address:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Source COM port:";
+            // 
+            // SysTrayIcon
+            // 
+            this.SysTrayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("SysTrayIcon.Icon")));
+            this.SysTrayIcon.Text = "Pinpoint Relay Station";
+            this.SysTrayIcon.Visible = true;
+            this.SysTrayIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.SysTrayIcon_MouseDoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(320, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(190, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 253);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.WebServerAddressBox);
+            this.Controls.Add(this.LogBox);
+            this.Controls.Add(this.COMPortsList);
+            this.Controls.Add(this.RefreshButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Pinpoint Relay Station";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RefreshButton;
+        private System.Windows.Forms.ListBox COMPortsList;
+        private System.Windows.Forms.TextBox LogBox;
+        private System.Windows.Forms.TextBox WebServerAddressBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NotifyIcon SysTrayIcon;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ PinPointRelay/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using System.IO;
+using System.Web;
+using System.Net;
+
+namespace PinPointRelay
+{
+    public partial class Form1 : Form
+    {
+        static string AppData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        static string PinpointFolder = Path.Combine(AppData, "Pinpoint");
+        static string PinpointLogFile = Path.Combine(PinpointFolder, "log.txt");
+        static string PinpointOptionsFile = Path.Combine(PinpointFolder, "options.txt");
+
+        string COMPortName { get; set; }
+        string WebServerAddress { get; set; }
+
+        Thread ReadThread { get; set; }
+        SerialPort sp { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            RefreshButton_Click(null, null);
+
+            try
+            {
+                LoadOptions();
+            }
+            catch { }
+
+            LogLine("Program started");
+
+            ReadThread = new Thread(Read);
+            ReadThread.Start();
+        }
+
+        /// <summary>
+        /// Continuously reads lines from the SerialPort object
+        /// and logs them in the LogBox and PinpointLogFile.
+        /// </summary>
+        void Read()
+        {
+            while (true)
+            {
+                try
+                {
+                    string line = sp.ReadLine().Replace("\n", "").Replace("\r", "");
+                    LogLine(line);
+                }
+                catch { }
+            }
+        }
+
+        /// <summary>
+        /// Refresh the COM ports listed in the COMPortsList
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            COMPortsList.Items.Clear();
+            foreach (string PortName in SerialPort.GetPortNames())
+            {
+                COMPortsList.Items.Add(PortName);
+            }
+
+            // If COMPortName is still contained in COMPortsList, select it
+            SelectCOMPort(COMPortName);
+        }
+
+        /// <summary>
+        /// Select a COM port by name. If it's in the COMPortsList,
+        /// it will be selected in the list and a SerialPort object
+        /// will be created for it.
+        /// </summary>
+        /// <param name="COMPort">Name of COM port. ex: COM4</param>
+        void SelectCOMPort(string COMPort)
+        {
+            int index = COMPortsList.FindString(COMPort);
+            if (index != -1)
+            {
+                COMPortsList.SetSelected(index, true);
+                // COM port is autmatically opened here due to the
+                // SelectedValueChanged event
+            }
+
+        }
+
+        /// <summary>
+        /// Opens the COM port named by COMPortName
+        /// </summary>
+        void OpenCOMPort()
+        {
+            if (sp != null && sp.IsOpen)
+            {
+                sp.Close();
+            }
+            sp = new SerialPort(COMPortName, 9600, Parity.None, 8, StopBits.One);
+            sp.Open();
+        }
+
+        private void COMPortsList_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (COMPortsList.SelectedIndex != -1)
+            {
+                string NewCOMPortName = (string)((ListBox)sender).SelectedItem;
+                if (NewCOMPortName != COMPortName)
+                {
+                    COMPortName = NewCOMPortName;
+                    OpenCOMPort();
+                    LogLine("COM port changed to " + COMPortName);
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Log a line in the LogBox textbox and in the 
+        /// PinpointLogFile text file.
+        /// </summary>
+        /// <param name="line"></param>
+        private void LogLine(string line)
+        {
+            string FullLine = DateTime.UtcNow.ToString("s") + " " + line + Environment.NewLine;
+            File.AppendAllText(PinpointLogFile, FullLine);
+
+            if (Thread.CurrentThread == ReadThread)
+            {
+                this.Invoke((MethodInvoker)delegate()
+                {
+                    LogBox.AppendText(FullLine);
+                });
+            }
+            else
+            {
+                LogBox.AppendText(FullLine);
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            ReadThread.Abort();
+            SaveOptions();
+            LogLine("Program terminated");
+        }
+
+        /// <summary>
+        /// Load options from PinpointOptionsFile
+        /// </summary>
+        void LoadOptions()
+        {
+            foreach (string line in File.ReadAllLines(PinpointOptionsFile))
+            {
+                try
+                {
+                    string optionname = line.Substring(0, line.IndexOf('='));
+                    string optionvalue = line.Substring(line.IndexOf('=') + 1);
+
+                    switch (optionname)
+                    {
+                        case "comport":
+                            SelectCOMPort(optionvalue);
+                            break;
+                        case "weburl":
+                            WebServerAddressBox.Text = optionvalue;
+                            break;
+                    }
+                }
+                catch { }
+            }
+        }
+
+        /// <summary>
+        /// Save options to PinpointOptionsFile
+        /// </summary>
+        void SaveOptions()
+        {
+            if (!Directory.Exists(PinpointFolder))
+            {
+                Directory.CreateDirectory(PinpointFolder);
+            }
+
+            File.WriteAllLines(PinpointOptionsFile, new string[] {
+                "comport=" + COMPortName,
+                "weburl=" + WebServerAddress
+            });
+        }
+
+        private void WebServerAddressBox_TextChanged(object sender, EventArgs e)
+        {
+            WebServerAddress = WebServerAddressBox.Text;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                this.Hide();
+                SysTrayIcon.Visible = true;
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                SysTrayIcon.Visible = false;
+                this.Show();
+            }
+        }
+
+        private void SysTrayIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            SysTrayIcon.Visible = false;
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+
+        private void SendLineToServer(string line)
+        {
+            // this is what we are sending
+            string post_data = "nmea=" + Uri.EscapeDataString(line);
+            // MessageBox.Show(post_data);
+
+            // this is where we will send it
+            string uri = WebServerAddressBox.Text;
+
+            // create a request
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.KeepAlive = false;
+            request.ProtocolVersion = HttpVersion.Version10;
+            request.Method = "POST";
+
+            // turn our request string into a byte stream
+            byte[] postBytes = Encoding.ASCII.GetBytes(post_data);
+
+            // this is important - make sure you specify type this way
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = postBytes.Length;
+            Stream requestStream = request.GetRequestStream();
+
+            // now send it
+            requestStream.Write(postBytes, 0, postBytes.Length);
+            requestStream.Close();
+
+            // grab te response and print it out to the console along with the status code
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string resp_str = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            //Console.WriteLine(response.StatusCode);
+            //MessageBox.Show(resp_str);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendLineToServer(textBox1.Text);
+            //SendLineToServer("$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47");
+        }
+    }
+}
